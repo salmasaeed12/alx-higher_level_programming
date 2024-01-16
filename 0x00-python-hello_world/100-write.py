0b8f262259e582ff5129e979b83888d3136ccda6
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2024-01-16 02:22:16 +0200

    python

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+
+# Write the message to stderr
+sys.stderr.write(message)
+
+# Exit with status code 1
+sys.exit(1)
+
