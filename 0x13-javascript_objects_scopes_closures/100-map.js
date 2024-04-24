b5da90f591a36064344baec179c661a830d2ea68
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2024-04-24 03:12:55 +0200

    MM

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
+const list = require('./100-data.js').list;
 
-const mylist = require('./100-data').list;
-
-console.log(mylist);
-console.log(mylist.map((item, index) => { return item * index; }));
-// map here is an itarator that itarates over the list, and the
-// first argument is first item of the list, second argument is its index and
-// the right side is the oparation that will be done on each item of the list
+const newList = list.map((val, idx) => val * idx);
+console.log(list);
+console.log(newList);
