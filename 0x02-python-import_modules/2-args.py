d00af86722839df403259c3e78e73e7d5e903e2a
Salma Saeed Mohamed <salmasa3ed12@gmail.com>
2024-06-22 22:06:29 +0300

    "10:06"

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-import sys
-a = len(sys.argv)
-if a == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(a-1))
-    for i in range(1, a):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    if (len(argv) - 1 == 0):
+        print("0 arguments.")
+    elif (len(argv) - 1 == 1):
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
